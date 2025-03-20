3366eab529c00b81ed58aef1b26001d153ed5a41
Wallis <Wallis@Wallis.mynetworksettings.com>
2025-03-20 17:42:46 -0400

    add project

---
+++ selenium-maven/src/main/java/Wallis/web/maven/selenium_maven/App.java
@@ -0,0 +1,13 @@
+package Wallis.web.maven.selenium_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
