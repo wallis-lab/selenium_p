708741ea5f2c8393019d724a88de2777e8ceeb48
wenlong <wangwenlong924@gmail.com>
2025-04-21 02:34:10 -0400

    123

--- selenium-maven/src/main/java/pageobject/CheckBoxPage.java
+++ selenium-maven/src/main/java/pageobject/CheckBoxPage.java
@@ -66,40 +66,45 @@ public class CheckBoxPage extends PageObject {
 
 	    WebElement MainLevel2 = getMainLevel2Element("//*[@id='bs_2']/span[1]");
 	    wait.until(ExpectedConditions.elementToBeClickable(MainLevel2));
-	    js.executeScript("arguments[0].scrollIntoView(true);", MainLevel2);
+	    js.executeScript("window.scrollBy(0, 300);"); // 模拟滚轮下滑
+	    Thread.sleep(500);
 	    MainLevel2.click();
 
 	    WebElement SubLevel4 = getSubLevel4Element("//*[@id='bf_4']/span[1]");
 	    wait.until(ExpectedConditions.elementToBeClickable(SubLevel4));
-	    js.executeScript("arguments[0].scrollIntoView(true);", SubLevel4);
+	    js.executeScript("window.scrollBy(0, 300);");
+	    Thread.sleep(500);
 	    SubLevel4.click();
 
 	    WebElement LastLevel16 = getLastLevel16Element("//*[@id=\"c_bf_4\"]");
-	    wait.until(ExpectedConditions.elementToBeClickable(LastLevel16));
-	    js.executeScript("arguments[0].scrollIntoView(true);", LastLevel16);
+	    wait.until(ExpectedConditions.visibilityOf(LastLevel16));
+	    js.executeScript("window.scrollBy(0, 300);");
+	    Thread.sleep(500);
 	    try {
-	        LastLevel16.click();  // Try clicking normally
+	        LastLevel16.click();
 	    } catch (ElementClickInterceptedException e) {
-	        js.executeScript("arguments[0].click();", LastLevel16);  // Fallback to JavaScript click
+	        js.executeScript("arguments[0].click();", LastLevel16);
 	    }
 
 	    WebElement LastLevel15 = getLastLevel15Element("//*[@id=\"c_io_15\"]");
 	    wait.until(ExpectedConditions.elementToBeClickable(LastLevel15));
-	    js.executeScript("arguments[0].scrollIntoView(true);", LastLevel15);
+	    js.executeScript("window.scrollBy(0, 300);");
+	    Thread.sleep(500);
 	    LastLevel15.click();
 
 	    WebElement LastLevel151 = getLastLevel151Element("//*[@id=\"c_io_16\"]");
-	    wait.until(ExpectedConditions.elementToBeClickable(LastLevel151));
-	    js.executeScript("arguments[0].scrollIntoView(true);", LastLevel151);
-	    Thread.sleep(500); // Wait for page stability
+	    wait.until(ExpectedConditions.visibilityOf(LastLevel151));
+	    js.executeScript("window.scrollBy(0, 300);");
+	    Thread.sleep(500);
 	    try {
-	        LastLevel151.click();  // Try clicking normally
+	        LastLevel151.click();
 	    } catch (ElementClickInterceptedException e) {
-	        js.executeScript("arguments[0].click();", LastLevel151);  // Fallback to JavaScript click
+	        js.executeScript("arguments[0].click();", LastLevel151);
 	    }
 
 	    return new CheckBoxPage(driver, baseUrl);
 	}
+
 
 
 
