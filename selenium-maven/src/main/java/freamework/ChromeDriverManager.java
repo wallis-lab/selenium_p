10a9bbe2d6cad61ac9623d232dce6e96012ba220
wenlong <wangwenlong924@gmail.com>
2025-04-21 11:16:00 -0400

    add headless not show the website

--- selenium-maven/src/main/java/freamework/ChromeDriverManager.java
+++ selenium-maven/src/main/java/freamework/ChromeDriverManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class ChromeDriverManager extends DriverManager{
 	private final String WEB_DRIVER_SYSTEM_PROPERTY_CHROME = "webdriver.chrome.driver";
 	private final String chromeDriverPath = "src\\resources\\chromedriver.exe";
+	private ChromeOptions options;
 
 	ChromeDriverManager(){
 		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY_CHROME, chromeDriverPath);
@@ -14,9 +15,13 @@ public class ChromeDriverManager extends DriverManager{
 
 	public ChromeDriverManager(ChromeOptions options) {
 		// TODO Auto-generated constructor stub
+		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY_CHROME, chromeDriverPath);
+        this.options = options;
 	}
 
 	public void createDriver() {
-		setDriver(new ChromeDriver());
+	
+            setDriver(new ChromeDriver(options));
+       
 	}
 }
